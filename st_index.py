3b56c417b696b9bc4e1226dfb15c471b4c67ae5a
Ed Saa <esaavedrac@u.northwestern.edu>
2025-05-06 17:41:31 -0500

    Edit theming

--- st_index.py
+++ st_index.py
@@ -16,10 +16,18 @@ st.html("assets/style.css")
 def warn_moved():
     st.info("**:blue[This site has moved to https://edsaac.me !]**", icon="🧹")
     st.image("https://placecats.com/neo_banana/300/200", caption="Not a cat", use_container_width=True)
+    st.link_button(
+        "**:rainbow-background[https://edsaac.me]**",
+        "https://edsaac.me",
+        help="Go to new site!",
+        icon="🔗",
+        use_container_width=True,
+        type="primary",
+    )
 
 
 warn_moved()
-cols = st.columns([0.4, 1], vertical_alignment="center")
+cols = st.columns([0.25, 1], vertical_alignment="center")
 with cols[0]:
     st.link_button(
         "**:rainbow-background[https://edsaac.me]**",
@@ -27,6 +35,7 @@ with cols[0]:
         help="Go to new site!",
         icon="🔗",
         use_container_width=True,
+        type="primary",
     )
 
 with cols[1]:
